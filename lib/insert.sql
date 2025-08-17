4e5ff3492167b91425494a98bf591a4e5d9b97c2
Your Name <you@example.com>
2025-08-17 05:54:04 -0700

    finished sql queries

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 7, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 6, 'F', 'Gray', 'Calm', 1),
+('Sergeant Brown', 12, 'M', 'Dark Brown', 'Loyal', 1),
+('Melissa', 5, 'F', 'White', 'Playful', 1),
+('Grinch', 8, 'M', 'Green', 'Grumpy', 0),
+('Wendy', 4, 'F', 'Golden', 'Friendly', 1),
+(NULL, 3, 'M', 'Black', 'Mysterious', 1);
